57c279ad093b360d8f869a5cabd5048edfbd3a88
ysk0423 <ssyhrsysk23@gmail.com>
2023-10-24 09:32:44 +0900

    feat: signin関数を作成

--- src/types/data.d.ts
+++ src/types/data.d.ts
@@ -1,12 +1,12 @@
-export const User = {
-  id: number,
-  username: string,
-  displayName: string,
-  email: string,
-  profileImageUrl: string,
-  description: string,
+export type User = {
+  id: number;
+  username: string;
+  displayName: string;
+  email: string;
+  profileImageUrl: string;
+  description: string;
 };
 
-export const ApiContext = {
-  apiRootUrl: string,
+export type ApiContext = {
+  apiRootUrl: string;
 };
